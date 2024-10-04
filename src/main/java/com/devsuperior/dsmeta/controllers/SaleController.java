4a373cbaf6e840b815905f85e346707f676e5480
Davidsolli <davidolisoa@gmail.com>
2024-10-03 22:31:10 -0300

    desafio 4 - consultas de vendas concluido

--- src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,14 +1,19 @@
 package com.devsuperior.dsmeta.controllers;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.services.SaleService;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -24,14 +29,17 @@ public class SaleController {
 	}
 
 	@GetMapping(value = "/report")
-	public ResponseEntity<?> getReport() {
-		// TODO
-		return null;
+	public ResponseEntity<Page<SaleReportDTO>> getReport(@RequestParam(name = "minDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate minDate,
+														 @RequestParam(name = "maxDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate maxDate,
+														 @RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+		Page<SaleReportDTO> saleDTO = service.findAllReports(minDate, maxDate, name, pageable);
+		return ResponseEntity.ok(saleDTO);
 	}
 
 	@GetMapping(value = "/summary")
-	public ResponseEntity<?> getSummary() {
-		// TODO
-		return null;
+	public ResponseEntity<List<SaleSummaryDTO>> getSummary(@RequestParam(name = "minDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate minDate,
+														  @RequestParam(name = "maxDate", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate maxDate) {
+		List<SaleSummaryDTO> saleSummaryDTOList = service.findAllSummary(minDate, maxDate);
+		return ResponseEntity.ok(saleSummaryDTOList);
 	}
 }
--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -1,8 +1,14 @@
 package com.devsuperior.dsmeta.services;
 
-import java.util.Optional;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import com.devsuperior.dsmeta.dto.SaleReportDTO;
+import com.devsuperior.dsmeta.dto.SaleSummaryDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
@@ -16,8 +22,32 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	public SaleMinDTO findById(Long id) {
-		Optional<Sale> result = repository.findById(id);
-		Sale entity = result.get();
-		return new SaleMinDTO(entity);
+		Sale result = repository.findById(id).orElseThrow();
+		return new SaleMinDTO(result);
+	}
+
+	public Page<SaleReportDTO> findAllReports(LocalDate minDate, LocalDate maxDate, String name, Pageable pageable) {
+
+		if (minDate == null) {
+			minDate = LocalDate.now().minusYears(2); // Coloquei 2 anos para apareçer alguma coisa
+		}
+		if (maxDate == null) {
+			maxDate =  LocalDate.now();
+		}
+
+		Page<Sale> salePage = repository.searchAllReports(minDate, maxDate, name, pageable);
+		return salePage.map(SaleReportDTO::new);
+	}
+
+	public List<SaleSummaryDTO> findAllSummary(LocalDate minDate, LocalDate maxDate) {
+
+		if (minDate == null) {
+			minDate = LocalDate.now().minusYears(2); // Coloquei 2 anos para apareçer alguma coisa
+		}
+		if (maxDate == null) {
+			maxDate =  LocalDate.now();
+		}
+
+        return repository.searchAllSummary(minDate, maxDate);
 	}
 }
